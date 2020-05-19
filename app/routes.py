e2d73b09bd2c4c1e50642d1674a82666cd348a49
otr0624 <otr0624@mail.com>
2020-05-19 14:37:11 -0400

    Added create patient form and template structure

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
 from flask import render_template
 from app import app
+from app.forms import PatientForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='HCA App Home')
+
+@app.route('/newpatient')
+def newpatient():
+    form = PatientForm()
+    return render_template('newpatient.html', title="Create Patient", form=form)
