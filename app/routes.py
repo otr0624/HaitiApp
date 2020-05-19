82aaff4141380f3aa7bc842f06b051661ced1958
otr0624 <otr0624@mail.com>
2020-05-19 14:49:34 -0400

    Added post functionality to patient form and flash message to confirm submission

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import PatientForm
 
@@ -8,7 +8,11 @@ from app.forms import PatientForm
 def index():
     return render_template('index.html', title='HCA App Home')
 
-@app.route('/newpatient')
+
+@app.route('/newpatient', methods=['GET', 'POST'])
 def newpatient():
     form = PatientForm()
+    if form.validate_on_submit():
+        flash('Post request submitted for new patient {} {}'.format(form.pt_first.data, form.pt_last.data))
+        return redirect('/index')
     return render_template('newpatient.html', title="Create Patient", form=form)
