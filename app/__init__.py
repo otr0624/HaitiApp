b1cfdd39d5a81ab9df23b809bc878f38f3326765
otr0624 <otr0624@mail.com>
2020-05-20 14:27:32 -0400

    Added functionality to save patient form data to sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from app.patients.patient import patient_bp
-from app.general.index import index_bp
+
 import config
 
 app = Flask(__name__)
@@ -10,8 +9,11 @@ app.config.from_object(config.DevelopmentConfig)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+from app.patients.patient import patient_bp
+from app.general.index import index_bp
+from app.patients.patient_model import Patient
+
 app.register_blueprint(patient_bp, url_prefix='/patient')
 app.register_blueprint(index_bp, url_prefix='/')
 
-from app.patients.patient_model import Patient
 
--- app/patients/patient.py
+++ app/patients/patient.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
+from app import db
 from app.patients.forms import CreatePatientForm
+from app.patients.patient_model import Patient
 
 patient_bp = Blueprint('patient_bp', __name__,
                        template_folder='templates',
@@ -11,7 +13,9 @@ patient_bp = Blueprint('patient_bp', __name__,
 def create_patient():
     form = CreatePatientForm()
     if form.validate_on_submit():
-        # Flash message as placeholder until MySQL connection created
-        flash('POST request submitted for new patient {} {}'.format(form.pt_first.data, form.pt_last.data))
+        patient = Patient(first_name=form.pt_first.data, last_name=form.pt_last.data, status=form.pt_type.data)
+        db.session.add(patient)
+        db.session.commit()
+        flash('Patient added successfully.')
         return redirect(url_for('index_bp.index'))
     return render_template('patient.html', title="Patient Dashboard", form=form)
