a93ff773f9f0ba7d585180a21bc9eabfe1f88c1c
Owen Robinson <otr0624@mail.com>
2020-06-08 14:14:29 -0400

    Refined random patient ID logic

--- app/id_gen.py
+++ app/id_gen.py
@@ -2,7 +2,7 @@ import random
 
 
 def rand_id(length=6):
-    char_set = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"  # removes I, O, 1, 0
+    char_set = "ABCDEFGHJKLMNPQRTUVWXYZ2346789"  # removes lookalikes I/1, O/0, 5/S
     return ''.join((random.choice(char_set) for i in range(length)))
 
 #  Once ID database implemented, add "if not in database" logic to enforce unique value
