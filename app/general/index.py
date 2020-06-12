14890ad2dfc765f5e4c3204321a98057caa482be
Owen Robinson <otr0624@mail.com>
2020-06-12 13:56:56 -0400

    Added summary stats to patient card on dashboard

--- app/general/index.py
+++ app/general/index.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, render_template
+from app.patients.patient_model import Patient
+from sqlalchemy import func
+
 
 index_bp = Blueprint('index_bp', __name__,
                      template_folder='templates',
@@ -9,4 +12,15 @@ index_bp = Blueprint('index_bp', __name__,
 @index_bp.route('/')
 @index_bp.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+    preop_count = Patient.query.filter_by(patient_status_id=2).count()
+    postop_count = Patient.query.filter_by(patient_status_id=3).count()
+    follow_count = Patient.query.filter_by(patient_status_id=4).count()
+    uncat_count = Patient.query.filter_by(patient_status_id=1).count()
+    patient_count = len(Patient.query.all())
+    return render_template('index.html',
+                           title='Home',
+                           patient_count=patient_count,
+                           preop_count=preop_count,
+                           postop_count=postop_count,
+                           follow_count=follow_count,
+                           uncat_count=uncat_count)
