1d7b409f516f961068f2c157ba0bc075a7163137
Owen Robinson <otr0624@mail.com>
2020-06-12 14:15:17 -0400

    Added provider stats to dashboard

--- app/general/index.py
+++ app/general/index.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from app.patients.patient_model import Patient
+from app.providers.provider_model import Provider
 from sqlalchemy import func
 
 
@@ -17,10 +18,22 @@ def index():
     follow_count = Patient.query.filter_by(patient_status_id=4).count()
     uncat_count = Patient.query.filter_by(patient_status_id=1).count()
     patient_count = len(Patient.query.all())
+
+    cards_count = Provider.query.filter_by(provider_category_id=2).count()
+    primdocnurse_count = Provider.query.filter_by(provider_category_id=3).count() + Provider.query.filter_by(provider_category_id=4).count()
+    socwork_count = Provider.query.filter_by(provider_category_id=5).count()
+    provider_count = len(Provider.query.all())
+    allotherprov_count = provider_count - (cards_count + primdocnurse_count + socwork_count)
+
     return render_template('index.html',
                            title='Home',
                            patient_count=patient_count,
                            preop_count=preop_count,
                            postop_count=postop_count,
                            follow_count=follow_count,
-                           uncat_count=uncat_count)
+                           uncat_count=uncat_count,
+                           cards_count=cards_count,
+                           primdocnurse_count=primdocnurse_count,
+                           socwork_count=socwork_count,
+                           allotherprov_count=allotherprov_count,
+                           provider_count=provider_count)
