2aca3b4686c1ad84bb6b49e5b7792ae74cff1010
Owen Robinson <otr0624@mail.com>
2020-06-15 13:56:58 -0400

    Added 404 route for pages under construction

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,7 @@ from app.patients.patient import patient_bp
 from app.providers.provider import provider_bp
 from app.facilities.facility import facility_bp
 from app.general.index import index_bp
+from app.general.construction import construction_bp
 from app.patients.patient_model import Patient, PatientStatus
 from app.providers.provider_model import Provider, ProviderCategory
 from app.facilities.facility_model import Facility, FacilityCategory
@@ -23,3 +24,4 @@ app.register_blueprint(index_bp, url_prefix='/')
 app.register_blueprint(patient_bp, url_prefix='/patients')
 app.register_blueprint(provider_bp, url_prefix='/providers')
 app.register_blueprint(facility_bp, url_prefix='/facilities')
+app.register_blueprint(construction_bp, url_prefix='/')
---
+++ app/general/construction.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+construction_bp = Blueprint('construction_bp', __name__,
+                     template_folder='templates',
+                     static_folder='static',
+                     static_url_path='assets')
+
+
+@construction_bp.route('/404')
+def construction():
+    return render_template('404.html')
