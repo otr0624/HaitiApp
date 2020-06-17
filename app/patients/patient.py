4c2413bc2e17b63a584a3ecb0309504bd50e5c6f
Owen Robinson <otr0624@mail.com>
2020-06-17 12:35:56 -0400

    Reverted form templating changes

--- app/patients/patient.py
+++ app/patients/patient.py
@@ -22,6 +22,7 @@ def view_patient_list():
 def create_patient():
     form = PatientProfileForm()
     patient_id = rand_id(6)
+    action = "Add"
     if form.validate_on_submit():
         patient = Patient(
             first_name=form.first_name.data,
@@ -34,7 +35,7 @@ def create_patient():
         db.session.commit()
         flash("Patient '{} {}' successfully created".format(form.first_name.data, form.last_name.data))
         return redirect(url_for('patient_bp.view_patient_list'))
-    return render_template('new-patient.html', title="Add Patient", form=form)
+    return render_template('patient_form/new-patient.html', title="Add Patient", form=form, action=action)
 
 
 @patient_bp.route('/delete/<string:patient_id>')
@@ -51,6 +52,7 @@ def edit_patient(patient_id):
     patient_obj = Patient.query.filter_by(patient_id=patient_id).first()
     #debugging app.logger.debug(f"patient to edit is {patient_obj}")
     form = PatientProfileForm(obj=patient_obj)
+    action = "Edit"
     if form.validate_on_submit():
         #debugging app.logger.debug(f"last name would be set to {form.last_name.data}, first {form.first_name.data}, stat {form.patient_status.data.id}")
         patient_obj.last_name = form.last_name.data
@@ -61,9 +63,9 @@ def edit_patient(patient_id):
         db.session.commit()
         flash("Patient '{} {}' successfully edited".format(form.first_name.data, form.last_name.data))
         return redirect(url_for('patient_bp.view_patient_list'))
-    return render_template('edit-patient.html',
+    return render_template('patient_form/edit-patient.html',
                            title="Edit Patient",
-                           form=form)
+                           form=form, action=action)
 
 
 @patient_bp.route('/view/<string:patient_id>')
