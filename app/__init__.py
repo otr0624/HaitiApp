2b49ee8b8e1e01068ee8ee3b2ad5686e95fde5c0
Owen Robinson <otr0624@mail.com>
2020-06-23 14:24:51 -0400

    Added dynamic field sandbox

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,7 @@ from app.providers.provider import provider_bp
 from app.facilities.facility import facility_bp
 from app.general.index import index_bp
 from app.general.construction import construction_bp
+from app.general.dynamic import dynamic_bp
 from app.patients.patient_model import Patient, PatientStatus
 from app.providers.provider_model import Provider, ProviderCategory
 from app.facilities.facility_model import Facility, FacilityCategory
@@ -25,3 +26,4 @@ app.register_blueprint(patient_bp, url_prefix='/patients')
 app.register_blueprint(provider_bp, url_prefix='/providers')
 app.register_blueprint(facility_bp, url_prefix='/facilities')
 app.register_blueprint(construction_bp, url_prefix='/')
+app.register_blueprint(dynamic_bp, url_prefix='/')
